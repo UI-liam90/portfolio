5632449d99996b4110a8b2d528201ae016a38344
uiliam90@gmail.com <lyoto90>
2020-02-16 15:54:33 +0000

    added scafhold of site

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -2,19 +2,31 @@ import React from "react"
 
 const Welcome = () => {
   const WelcomeMessage = () => {
-    const time = new Date();
-    const hour = time.getHours();
+    let time = new Date();
+    let hour = time.getHours();
     if(hour >= 0 && hour < 12) {
-      return `Good Morning ${hour}`
+      return `Good Morning`
     } else if(hour >= 12 && hour < 18) {
-      return `Good Afternoon ${hour}`
+      return `Good Afternoon`
     } else {
-      return `Good evening ${hour}`
+      return `Good evening`
     }
+  }
+  const NiceMessage = () => {
+    const greetings = [
+      'How are you Today?',
+      'I hope you are having a great day :)',
+      'How can I help you on this fine day?'
+    ]
+    const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+
+    return greeting;
   }
   return (
     <>
       <h1><WelcomeMessage /></h1>
+      <h2>My Name is Liam Wright</h2>
+      <h3><NiceMessage /></h3>
     </>
   );
 }
